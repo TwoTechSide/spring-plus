448a30a897f370738b6c491850451d3768d9eb84
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-22 09:37:18 +0900

    Lv.2-7: N+1 -> JOIN FETCH

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // Lv.2-7: commentList를 가져온 뒤 comment.getUser()를 사용하면서 N+1 문제 발생
+    // -> JOIN FETCH를 사용하여 연관된 데이터를 함께 가져오면서 N+1 문제 해결
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
